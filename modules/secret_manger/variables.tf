f78580f33d6609e7439b6fbc23c3e27a79a2c052
Hossamgit447 <hossamgithub@gmail.com>
2025-08-25 17:33:38 -0700

    variables defaults removed

--- modules/secret_manger/variables.tf
+++ modules/secret_manger/variables.tf
@@ -1,13 +1,11 @@
 variable "name" {
   description = "Name of the secret"
   type        = string
-  default = "myapp-db-password"
 }
 
 variable "description" {
   description = "Description of the secret"
   type        = string
-  default     = null
 }
 
 
