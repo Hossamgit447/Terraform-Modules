169e10cf9b4fd511d7252e78d8ca35a44243bea6
Hossamgit447 <hossamgithub@gmail.com>
2025-08-25 17:20:54 -0700

    a

--- modules/network/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-}
-
-variable "env" {
-  description = "Environment (dev/staging/prod)"
-  type        = string
-}
--- modules/secret_manger/main.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_secretsmanager_secret" "this" {
-  name        = var.name
-  description = var.description
-  
-}
