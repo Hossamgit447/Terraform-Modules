cb52499810c3d54426c0a1c7f5cab3aa24be6d73
Hossamgit447 <hossamgithub@gmail.com>
2025-08-26 18:01:01 -0700

    dub code removed

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -11,10 +11,3 @@ resource "aws_eks_cluster" "this" {
   }
 }
 
-output "eks_cluster_endpoint" {
-  value = aws_eks_cluster.this.endpoint
-}
-
-output "eks_cluster_certificate" {
-  value = aws_eks_cluster.this.certificate_authority[0].data
-}
