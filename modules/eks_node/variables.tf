8563a4e282db8163af6803e624dd5614b9ac9343
Hossamgit447 <hossamgithub@gmail.com>
2025-08-28 21:33:11 -0700

    vars declared

--- modules/eks_node/variables.tf
+++ modules/eks_node/variables.tf
@@ -2,11 +2,6 @@ variable "env" {
   description = "Environment name (e.g., dev, staging, prod)"
   type        = string
 }
-
-
-
-
-
 variable "private_subnet_ids" {
   description = "List of private subnet IDs where EKS cluster resources can be placed"
   type        = list(string)
@@ -16,4 +11,23 @@ variable "node_role_arn" {
   description = "node role arn"
   type = string
   default = null
+}
+
+variable "cluster_name" {
+  type        = string
+}
+variable "subnet_ids" {
+  type        = list(string)
+}
+variable "desired_size" {
+  type        = number
+}
+variable "min_size" {
+  type        = number
+}
+variable "max_size" {
+  type        = number
+}
+variable "instance_types" {
+  type = string
 }
