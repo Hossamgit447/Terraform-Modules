97c4da90e9eb67eedea0553e1dad381452b37515
Hossamgit447 <hossamgithub@gmail.com>
2025-08-28 21:11:11 -0700

    minor changes

--- modules/eks_node/main.tf
+++ modules/eks_node/main.tf
@@ -1,15 +1,4 @@
-resource "aws_eks_cluster" "this" {
-  name     = "${var.env}-eks-cluster"
-  role_arn = var.cluster_role_arn
 
-  vpc_config {
-    subnet_ids = concat(var.public_subnet_ids, var.private_subnet_ids)
-  }
-
-  tags = {
-    Name = "${var.env}-eks"
-  }
-}
 
 resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
--- modules/eks_node/variables.tf
+++ modules/eks_node/variables.tf
@@ -3,16 +3,9 @@ variable "env" {
   type        = string
 }
 
-variable "cluster_role_arn" {
-  description = "IAM Role ARN for the EKS cluster (must have eks.amazonaws.com trust relationship)"
-  type        = string
-}
 
-variable "public_subnet_ids" {
-  description = "List of public subnet IDs where EKS cluster resources can be placed"
-  type        = list(string)
-  default     = []
-}
+
+
 
 variable "private_subnet_ids" {
   description = "List of private subnet IDs where EKS cluster resources can be placed"
