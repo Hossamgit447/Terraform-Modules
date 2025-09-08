a0b6745e187513146b54e481b3ac87578028d108
Hossamgit447 <hossamgithub@gmail.com>
2025-09-08 08:15:06 -0700

    aaa

---
+++ modules/nat/main.tf
@@ -0,0 +1,29 @@
+resource "aws_eip" "nat" {
+  vpc = true
+  tags = { Name = "${var.env}-nat-eip" }
+}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = var.public_subnet_id
+  tags          = { Name = "${var.env}-nat" }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = var.vpc_id
+  tags   = { Name = "${var.env}-private-rt" }
+}
+
+# Default route out through NAT
+resource "aws_route" "private_default" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.this.id
+}
+
+# One association per private subnet
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnet_ids)
+  subnet_id      = var.private_subnet_ids[count.index]
+  route_table_id = aws_route_table.private.id
+}
