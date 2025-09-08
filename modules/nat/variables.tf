751abaff54ca9f836ad6d724a4476e8bdbeebbac
Hossamgit447 <hossamgithub@gmail.com>
2025-09-08 08:22:42 -0700

    aaa

--- modules/nat/variables.tf
+++ modules/nat/variables.tf
@@ -1,6 +1,6 @@
 variable "env"       { type = string }
 variable "vpc_id"    { type = string }
-variable "public_subnet_ids" { type = string } # NAT must live in a public subnet
+variable "public_subnet_ids" { type = list(string) } # NAT must live in a public subnet
 variable "private_subnet_ids" {
   type = list(string)
 }
