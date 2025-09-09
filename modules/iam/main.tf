4dcc5c02bef043c4636ec709cafb5934d6cff397
Hossamgit447 <hossamgithub@gmail.com>
2025-09-09 06:04:12 -0700

    asdf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -56,33 +56,3 @@ resource "aws_iam_role_policy_attachment" "eks_node_AmazonEKS_CNI_Policy" {
 }
 
 # IAM Policy for ALB Controller
-data "aws_iam_policy_document" "alb_controller" {
-  statement {
-    actions   = ["sts:AssumeRoleWithWebIdentity"]
-    effect    = "Allow"
-    principals {
-      type        = "Federated"
-      identifiers = [var.oidc_provider_arn]
-    }
-    condition {
-      test     = "StringEquals"
-      variable = "${replace(var.oidc_provider_arn, "arn:aws:iam::", "")}:sub"
-      values   = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
-    }
-  }
-}
-
-resource "aws_iam_role" "alb_controller" {
-  name               = "${var.env}-alb-controller"
-  assume_role_policy = data.aws_iam_policy_document.alb_controller.json
-}
-
-resource "aws_iam_role_policy_attachment" "alb_controller" {
-  role       = aws_iam_role.alb_controller.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSLoadBalancerControllerIAMPolicy"
-}
-resource "aws_iam_openid_connect_provider" "this" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.aws_eks_cluster.this.certificate_authority[0].data]
-  url             = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
-}
