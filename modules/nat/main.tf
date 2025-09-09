ee4e55dfb70adc2647c200f3b461be0fa28a0179
Hossamgit447 <hossamgithub@gmail.com>
2025-09-08 20:20:52 -0700

    sfdg

--- modules/nat/main.tf
+++ modules/nat/main.tf
@@ -19,6 +19,7 @@ resource "aws_route" "private_default" {
   route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.this.id
+  depends_on = [aws_nat_gateway.this]
 }
 
 # One association per private subnet
