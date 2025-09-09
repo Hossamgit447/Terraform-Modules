ce699f1fd08565b178bb09af7226e8a98a817b97
Hossamgit447 <hossamgithub@gmail.com>
2025-09-08 20:40:23 -0700

    asd

--- modules/nat/main.tf
+++ modules/nat/main.tf
@@ -28,3 +28,28 @@ resource "aws_route_table_association" "private" {
   subnet_id      = var.private_subnet_ids[count.index]
   route_table_id = aws_route_table.private.id
 }
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = var.vpc_id
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = var.vpc_id
+  tags = {
+    Name = "${var.env}-public-rt"
+  }
+}
+
+resource "aws_route" "public_default" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_ids)
+  subnet_id      = var.public_subnet_ids[count.index]
+  route_table_id = aws_route_table.public.id
+}
